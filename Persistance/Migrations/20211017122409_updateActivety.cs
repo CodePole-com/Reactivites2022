0fc76ca12c724f515de77024c0a46682163f0082
Amr Wafa <amrwafa75@gmail.com>
2021-10-18 09:17:18 +0300

    end of section 2

---
+++ Persistance/Migrations/20211017122409_updateActivety.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.Migrations
+{
+    public partial class updateActivety : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Aactivites",
+                table: "Aactivites");
+
+            migrationBuilder.RenameTable(
+                name: "Aactivites",
+                newName: "Activities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities");
+
+            migrationBuilder.RenameTable(
+                name: "Activities",
+                newName: "Aactivites");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Aactivites",
+                table: "Aactivites",
+                column: "Id");
+        }
+    }
+}
